d3a054911185b5f7dc4fde453dec3d514aeee828
Marlleyck Nathan <marlleyck@hotmail.com>
2022-05-23 21:44:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,37 @@ function calcular() {
     let result = document.getElementById('if')
 
     let res = document.getElementById('res')
-    let p = document.createElement('p')
-    res.appendChild(p)
 
     let imc = peso / (altura ** 2)
 
-    res = ''
+    let strImc = String(imc)
+    let rImc
+    let iImc
+    let fImc
+    let tImc
+    let nImc
+    if (strImc.indexOf('.') > -1) {
+        rImc = strImc.replace('.', '')
+        iImc = rImc.slice(0, 2).concat('.')
+        fImc = rImc.substr(3, 1)
+        tImc = `${iImc}${fImc}`
+        nImc = Number(tImc)
+    }
+
+
+    res.innerHTML = `<p> ${nImc} </p>`
 
     if (peso == '' || altura == '' || nome == '') {
         alert('Verifique os dados e tente novamente!')
-    } else if (imc >= 0 && imc < 18.5) {
-        p.innerHTML = imc.toFixed(1)
+    } else if (nImc >= 0 && nImc < 18.5) {
         result.innerHTML = 'Abaixo do peso'
-    } else if (imc >= 18.5 && imc <= 24.9) {
-        p.innerHTML = imc.toFixed(1)
+    } else if (nImc >= 18.5 && nImc <= 24.9) {
         result.innerHTML = 'Peso normal'
-    } else if (imc >= 25 && imc <= 29.9) {
-        p.innerHTML = imc.toFixed(1)
+    } else if (nImc >= 25 && nImc <= 29.9) { 
         result.innerHTML = 'Sobrepeso'
-    } else if (imc >= 30 && imc <= 39.9) {
-        p.innerHTML = imc.toFixed(1)
+    } else if (nImc >= 30 && nImc <= 39.9) {
         result.innerHTML = 'Obesidade'
-    } else if (imc > 40) {
-        p.innerHTML = imc.toFixed(1)
+    } else if (nImc > 40) {
         result.innerHTML = 'Obesidade mórbida'
     } else {
         alert('Erro, tente novamente!')
